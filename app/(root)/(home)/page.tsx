74d9d81aff5206d177c0085bea2925ece412fbac
Sean Currlin <sean.currlin@gmail.com>
2024-05-23 14:29:15 -0400

    time zone fix 6

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,8 +3,10 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  const timeZone = process.env.TIMEZONE || 'America/New_York';
+
+  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone });
+  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeZone })).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
@@ -16,6 +18,7 @@ const Home = () => {
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
             <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+            <p className="text-lg font-medium text-sky-1 lg:text-xl">EST</p>
           </div>
         </div>
       </div>
